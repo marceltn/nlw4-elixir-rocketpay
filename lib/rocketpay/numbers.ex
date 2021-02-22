e1d31be5e582bef3afcc56d2c15f7d0b03948d13
Marcel <marcel.nakamine@outlook.com>
2021-02-22 09:02:11 -0300

    implement handle file logic

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -5,7 +5,16 @@ defmodule Rocketpay.Numbers do
     |> handle_file
   end
 
-  defp handle_file({:ok, file}), do: file
+  defp handle_file({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(fn number -> String.to_integer(number) end)
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+
   defp handle_file({:error, _reason}), do: {:error, "File does not exist!"}
 
 end
