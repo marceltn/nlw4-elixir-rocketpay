3d5d2c558bc3e2a9b51be4831388e98a3d187666
Marcel Teruo Nakamine <marcel.nakamine@outlook.com>
2021-02-22 09:26:11 -0300

    Day1 rumo ao proximo nivel (#1)
    * simple handle csv
    
    * using pattern matching
    
    * implement handle file logic
    
    * adding route params as filename
    
    * handle route to call sum from file method from Numbers module
    
    * use Stream to convert to number instead
    
    * implement unit test
    
    Co-authored-by: spacedog <spacedog@spacedog.local>

--- lib/rocketpay_web/controllers/welcome_controller.ex
+++ lib/rocketpay_web/controllers/welcome_controller.ex
@@ -1,8 +1,23 @@
 defmodule RocketpayWeb.WelcomeController do
   use RocketpayWeb, :controller
 
-  def index(conn, _params) do
+  alias Rocketpay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
     conn
-      |> text("Welcome to the Rocketpay API")
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to Rocketpay API. Here is your number: #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(%{message: reason})
   end
 end
