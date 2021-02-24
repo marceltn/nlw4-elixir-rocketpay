218fd193026471085ad64b0ac14f0b5f1a67fdaf
Marcel <marcel.nakamine@outlook.com>
2021-02-23 21:41:50 -0300

    create users migration

---
+++ priv/repo/migrations/20210224003842_create_user_table.exs
@@ -0,0 +1,13 @@
+defmodule Rocketpay.Repo.Migrations.CreateUserTable do
+  use Ecto.Migration
+
+  def change do
+    create table :users do
+      add :name, :string
+      add :age, :integer
+      add :email, :string
+      add :password_hash, :string
+      add :nickname, :string
+    end
+  end
+end
