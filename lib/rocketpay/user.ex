046558ff3726740334d8c6155f79faa8606773a4
Marcel <marcel.nakamine@outlook.com>
2021-02-23 22:15:26 -0300

    adding more validations for user

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -18,5 +18,10 @@ defmodule Rocketpay.User do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
+    |> validate_length(:password_hash, min: 6)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+    |> unique_constraint([:nickname])
   end
 end
