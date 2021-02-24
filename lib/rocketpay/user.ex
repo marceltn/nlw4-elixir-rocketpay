a26782388d0bbba0c45fe38be4d833f20b70fa52
Marcel <marcel.nakamine@outlook.com>
2021-02-23 22:28:26 -0300

    hashing the password

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -2,14 +2,17 @@ defmodule Rocketpay.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
   @primary_key {:id, :binary_id, autogenerate: true}
 
-  @required_params [:name, :age, :email, :password_hash, :nickname]
+  @required_params [:name, :age, :email, :password, :nickname]
 
   schema "users" do
     field :name, :string
     field :age, :integer
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :nickname, :string
   end
@@ -18,10 +21,17 @@ defmodule Rocketpay.User do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
-    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:password, min: 6)
     |> validate_number(:age, greater_than_or_equal_to: 18)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint([:email])
     |> unique_constraint([:nickname])
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Bcrypt.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
