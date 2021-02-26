0214be2e911c0b5498ed54feb1e776c95b767ddb
Marcel Teruo Nakamine <marcel.nakamine@outlook.com>
2021-02-26 00:47:52 -0300

    Day4 aprofundando nosso conhecimento (#4)
    * typo
    
    * accounts view
    
    * handle account errors on fallback
    
    * adding explicitly the decimal dependency
    
    * withdraw
    
    * refactor code
    
    * transaction
    
    * specify the transaction names
    
    * transaction view
    
    * TransactionResponse struct

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -1,7 +1,8 @@
-defmodule RocketpayWeb.AccountController do
+defmodule RocketpayWeb.AccountsController do
   use RocketpayWeb, :controller
 
   alias Rocketpay.Account
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
 
   action_fallback RocketpayWeb.FallbackController
 
@@ -10,6 +11,22 @@ defmodule RocketpayWeb.AccountController do
       conn
       |> put_status(:ok)
       |> render("update.json", account: account)
+    end
+  end
+
+  def withdraw(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
+  end
+
+  def transaction(conn, params) do
+    with {:ok, %TransactionResponse{} = transaction} <- Rocketpay.transaction(params) do
+      conn
+      |> put_status(:ok)
+      |> render("transaction.json", transaction: transaction)
     end
   end
 end
