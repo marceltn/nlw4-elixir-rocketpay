9be3a918faf6b7b90298f7d91c971738c6bb3d46
Marcel <marcel.nakamine@outlook.com>
2021-02-26 00:45:07 -0300

    TransactionResponse struct

--- lib/rocketpay_web/views/accounts_view.ex
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -1,5 +1,6 @@
 defmodule RocketpayWeb.AccountsView do
   alias Rocketpay.Account
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
 
   def render("update.json", %{account: %Account{id: account_id, balance: balance}}) do
     %{
@@ -12,7 +13,7 @@ defmodule RocketpayWeb.AccountsView do
   end
 
   def render("transaction.json", %{
-        transaction: %{to_account: to_account, from_account: from_account}
+        transaction: %TransactionResponse{to_account: to_account, from_account: from_account}
       }) do
     %{
       message: "Balance changed successfully",
